0489b8f3b346dd9f008082a837542a9611f95236
coreytegeler <coreytegeler@gmail.com>
2019-06-14 21:51:26 -0400

    averaging test results

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,41 +6,46 @@ var SPREADSHEET_ID = "1RjOpiMjIEZ2_NCrAMBqCSFj-3sYQu8NHhI1FsLYStjA";
 
 var testNames = ['Microbiometer', 'Brix', 'Ph', 'Temperature', 'Moisture', 'Nodules', 'Rhizosheaths'],
 		data = {},
-		beds = {};
+		tests = {},
+		graphs = {},
+		averages = {};
 
 
-function Bed(number) {
-	this.number = number;
+
+function Graph(id) {
+	this.id = id;
 	this.tests = {};
 	this.graph = {};
+	graphs[id] = this;
 }
 
 
-Bed.prototype.createTestGraph = function(testName) {
-	var bed = this,
-			number = bed.number,
-			graphData = bed.tests[testName];
+Graph.prototype.createTestGraph = function(testName) {
+	var graph = this,
+			id = graph.id,
+			testSlug = slugify(testName),
+			graphData = graph.tests[testName];
 
-	var bedSection = d3.select("section#bed-"+number);
-	if(!bedSection.size()) {
-		bedSection = d3.select("main")
+	var graphSection = d3.select("section#graph-"+id);
+	if(!graphSection.size()) {
+		graphSection = d3.select("main")
 			.append("section")
-				.attr("class", "bed")
-				.attr("id", "bed-"+number);
-		bedSection.append("h2")
-			.attr("class", "bed-title")
-			.text("Bed No. "+number);
+				.attr("class", "graph")
+				.attr("id", "graph-"+id);
+		graphSection.append("h2")
+			.attr("class", "graph-title")
+			.text(id);
 	}
 
-	var graphGrid = bedSection.select(".graph-grid");
+	var graphGrid = graphSection.select(".graph-grid");
 	if(!graphGrid.size()) {
-		graphGrid = bedSection
+		graphGrid = graphSection
 			.append("div")
 			.attr("class", "graph-grid row");
 	}
 
 	var col = graphGrid.append("div")
-		.attr("class", "graph-col col-12 col-sm-6");
+		.attr("class", "graph col col-12 col-sm-6 "+testSlug);
 	col.append("h3")
 		.attr("class", "graph-title")
 		.text(testName);
@@ -48,15 +53,16 @@ Bed.prototype.createTestGraph = function(testName) {
 		.attr("class", "graph-wrapper");
 
 	var wrapperNode = wrapper.node();
-	bed.graph.wrapper = wrapperNode;
+	graph.graph.wrapper = wrapperNode;
 
 	var margin = {top: 20, right: 20, bottom: 30, left: 40};
 	var wrapperWidth = wrapperNode.clientWidth,
 			wrapperHeight = wrapperNode.clientHeight;
 	var width = wrapperWidth - margin.left - margin.right,
 			height = wrapperHeight - margin.top - margin.bottom;
+
 	var svg = wrapper.append("svg")
-		.attr('id', number+'-'+testName)
+		.attr('id', id+'-'+testName)
 		.attr('width', width)
 		.attr('height', height)
 		.attr("preserveAspectRatio", "xMinYMin meet")
@@ -71,18 +77,22 @@ Bed.prototype.createTestGraph = function(testName) {
 	var zoom = d3.zoom()
 			.scaleExtent([1, 32])
 			.translateExtent([[0, 0], [width, height]])
-			.extent([[0, 0], [width, height]]);
-			// .on("zoom", function() {
-				// var t = d3.event.transform,
-				// 		xt = t.rescaleX(x);
-				// g.select(".area").attr("d", area.x(function(d) {
-				// 	return xt(d.date);
-				// }));
-				// g.select(".axis--x").call(xAxis.scale(xt));
-			// });
+			.extent([[0, 0], [width, height]])
+			.on("zoom", function() {
+				var t = d3.event.transform,
+						xt = t.rescaleX(x);
+				g.select(".area").attr("d", area.x(function(d) {
+					return xt(d.date);
+				}));
+				g.select(".axis--x").call(xAxis.scale(xt));
+			});
 
 	var area = d3.area()
-			// .curve(d3.curveMonotoneX)
+			// .curve(d3.curveCatmullRom)
+			.curve(d3.curveLinear)
+			// .curve(d3.curveStepBefore)
+			// .curve(d3.curveStepAfter)
+			// .curve(d3.curveStep)
 			.x(function(d) { return x(d.date); })
 			.y0(height)
 			.y1(function(d) { return y(d.value); });
@@ -92,7 +102,7 @@ Bed.prototype.createTestGraph = function(testName) {
 		.append("rect")
 			.attr("width", width)
 			.attr("height", height);
-	bed.graph.svg = svg;
+	graph.graph.svg = svg;
 
 	var g = svg.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -116,6 +126,18 @@ Bed.prototype.createTestGraph = function(testName) {
 
 	svg.call(zoom);
 
+	var buttonsWrapper = col.append("div")
+		.attr("class", "buttons-wrapper");
+
+	var averageToggle = buttonsWrapper.append("div")
+		.attr("class", "button toggle average-toggle")
+		.text("Compare to average");
+
+	var downloadButton = buttonsWrapper.append("div")
+		.attr('href', '#')
+		.attr("class", "button toggle download-data")
+		.text("Download data");
+
 	// window.addEventListener("resize", function() {
 	// 	var svg = bed.graph.svg,
 	// 			wrapper = bed.graph.wrapper,
@@ -135,59 +157,103 @@ Bed.prototype.createTestGraph = function(testName) {
 
 }
 
-Bed.prototype.addTestData = function(testName, entries) {
+Graph.prototype.addTestData = function(testName, entries) {
 	this.tests[testName] = entries;
 	this.createTestGraph(testName);
+}
+
+function averageData(testName) {
+	var test = tests[testName];
+	var dates = {};
+	var sum = 0;
+	test.forEach(function(entry, i) {
+		if(!dates[entry.date]) {
+			dates[entry.date] = [];
+		}
+		sum += entry.value;
+		dates[entry.date].push(entry.value);
+	});
+	var orderedDates = [],
+			orderedValues =[];
+	Object.keys(dates).forEach(function(date, i){
+		orderedDates.push(new Date(date));
+	});
+	orderedDates.sort(function(a, b) {
+		return new Date(b) - new Date(a);
+	});
+	orderedDates.forEach(function(date, i) {
+		var values = dates[date],
+				sum = 0;
+		values.forEach(function(value, j) {
+			sum += value;
+		});
+		var avg = sum/values.length;
+		orderedValues[i] = {
+			date: date,
+			value: avg
+		}
+	});
+	var graphId = 'avg';
+	var avgGraph = new Graph(testName);
+	avgGraph.id = graphId;
+	avgGraph.tests[testName] = orderedValues;
+	avgGraph.createTestGraph(testName);
+	graphs[graphId] = avgGraph;
+	// avgGraph
+	// avgGraph.addTestData('avg');
+	// addTestData
 }
 
 function handleData(testName, testData) {
 	var parseDate = d3.timeParse("%b %Y");
 	var headers = testData.shift();
 	headers.shift();
+
+	if(!tests[testName]) {
+		tests[testName] = [];
+	}
 	testData.forEach(function(bedRowData, i) {
-		var bedNum = bedRowData.shift();
-		if(!beds[bedNum]) {
-			beds[bedNum] = new Bed(bedNum);	
+		var graphId = bedRowData.shift();
+		if(!graphs[graphId]) {
+			graphs[graphId] = new Graph(graphId);	
 		}
-		var bed = beds[bedNum];
+		var graph = graphs[graphId];
 		var entries = [];
 		bedRowData.forEach(function(testValue, j) {
 			var date = new Date(headers[j]);
 			var value = testValue ? parseInt(testValue) : 0;
+			if(!value) {return}
 			var entry = {
 				date: date,
 				value: value
 			};
 			entries.push(entry);
+			tests[testName].push(entry)
 		});
-		bed.tests[testName] = entries;
-		bed.createTestGraph(testName);
+		graph.tests[testName] = entries;
+		graph.createTestGraph(testName);
 	});
 }
 
-function makeApiCall(testName) {
+function getTestData(testName) {
 	var range = testName+"!A1:ZZ1000";
 	var params = {
 		spreadsheetId: SPREADSHEET_ID,
 		range: range,
 		majorDimension: "ROWS"
 	};
-
 	var request = gapi.client.sheets.spreadsheets.values.get(params);
 	request.then(function(response) {
 		var testData = response.result.values;
 		handleData(testName, testData);
+		averageData(testName, testData);
+		// if(Object.keys(tests).length === testNames.length) {
+		// 	averageData(testName, testData);
+		// }
 	}, function(reason) {
 		console.error("error: " + reason.result.error.message);
 	});
 }
-
-function loopTests() {
-	testNames.forEach(function(testName, i) {
-		makeApiCall(testName);
-	});
-}
-
 
 function initClient() {
 	gapi.client.init({
@@ -196,7 +262,9 @@ function initClient() {
 		"scope": SCOPE,
 		"discoveryDocs": ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
 	}).then(function() {
-		loopTests();
+		testNames.forEach(function(testName, i) {
+			getTestData(testName);
+		})
 	});
 }
 
